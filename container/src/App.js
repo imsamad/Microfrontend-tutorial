69793d89c7c9a00a2ec0e814134d5664d61d869f
Abdus Samad <imsamad00@gmail.com>
2023-09-16 12:34:44 +0530

    added

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
   return (
     <div>
-      <h2>Containerc!s!!!</h2>
+      <h2>Containderc!s!!!</h2>
       <MarketingApp />
     </div>
   );
